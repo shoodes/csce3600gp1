d46bb4d47e708d6018e63d262db8c16e95b19f2f
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-02-16 05:43:56 +0000

    Update parity.c
    updated to have unsigned integers

--- parity.c
+++ parity.c
@@ -1,28 +1,30 @@
-#include "major1.h"
+int Parity(unsigned int input);
+
+#include "major1.h
 
 int main(){
-	        printf("Enter your input value");// print statement
-	        int user_input;// var for userinput
-	        int *p = &user_input; //pointer
-	        scanf("%d",p);//scan
-	        printf("Your input value is: %d\n", user_input); //test
-	        Parity(user_input);//call to method
+	printf("Enter your input value");
+	int user_input;
+	int *p = &user_input;
+	scanf("%d",p);
+	printf("Your input value is: %d\n", user_input); //test
+	Parity(user_input);
 }
 
-int Parity(int input){
-	        int binary[8]; //binary array integer
-	        int parityCheck = 0; // used to check parity,
-        	for(int i = 0; i<=7; i++){
-		        binary[i] = input%2;
-		        input = input/2;
-		        parityCheck = parityCheck ^ binary[i];
-	        }
-	        if(parityCheck == 0){
-		        printf("Your input value has an even parity.\n");
-		        return 0;
-	        }
-	        else{
-		        printf("Your input value has an odd parity.\n");
-		        return 1;
-	        }
+int Parity(unsigned int input){
+	unsigned int binary[8]; 
+	unsigned int parityCheck = 0;
+	for(int i = 0; i<=7; i++){
+		binary[i] = input%2;
+		input = input/2;
+		parityCheck = parityCheck ^ binary[i];
+	}
+	if(parityCheck == 0){
+		printf("Your input value has an even parity.\n");
+		return 0;
+	}
+	else{
+		printf("Your input value has an odd parity.\n");
+		return 1;
+	}
 }
