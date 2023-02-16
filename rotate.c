9cfc2543bbd7bea10982d7d18d1933e12f73f659
shoodes <34947268+shoodes@users.noreply.github.com>
2023-02-16 11:48:49 -0600

    Add files via upload
    Changed 
    int main ()
    to
    void rotate ()
    
    Removed
    return 0;

--- rotate.c
+++ rotate.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+#include "major1.h"
 
-unsigned int rotate(unsigned int num, unsigned int rot) {
-    rot %= 32;  // ensure rot is between 0 and 31
+unsigned int rotates(unsigned int num, unsigned int rot) {
+    //rot %= 32;  // ensure rot is between 0 and 31
     return (num >> rot) | (num << (32 - rot));
 }
 
-int main() {
+void rotate() {
     unsigned int num, rot;
-    printf("Enter a number to rotate between  1  and  4294967295: ");
+    printf("Enter a 32-bit number (>= 1 and <= 4294967295, inclusively):  ");
     scanf("%u", &num);
     printf("Enter number of bits to rotate: ");
     scanf("%u", &rot);
-    unsigned int result = rotate(num, rot);
+    unsigned int result = rotates(num, rot);
     printf("Result: %u\n", result);
-    return 0;
 }
